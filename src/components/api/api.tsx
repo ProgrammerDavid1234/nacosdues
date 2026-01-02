c7129d79021f75ee06c2bb9aee6af4b3f429c229
Shady776 <temitayosalisu42@gmail.com>
2026-01-02 18:55:48 +0100

    sorry

--- src/components/api/api.tsx
+++ src/components/api/api.tsx
@@ -1,2 +1,2 @@
-// export const API_BASE_URL = 'https://hci-nacos-production.up.railway.app';
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://hci-nacos-production.up.railway.app';
+// export const API_BASE_URL = 'http://localhost:8000';
